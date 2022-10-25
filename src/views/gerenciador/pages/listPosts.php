06d15ff7d65124f3cd4309f267d8506f8073ffed
almirmurad <q>
2022-10-25 00:24:30 -0300

    melhoria que faltou

--- src/views/gerenciador/pages/listPosts.php
+++ src/views/gerenciador/pages/listPosts.php
@@ -20,7 +20,7 @@
                                 <th>ID</th>
                                 <th>Título</th>
                                 <th>Descrição</th>
-                                <th>Capa</th>
+                                <!-- <th>Capa</th> -->
                                 <th>Redator</th>
                                 <th>Criado em</th>
                                 <th>Ações</th>
@@ -28,16 +28,16 @@
                             <?php foreach($posts as $post): ?>
                             <tr>
                                     
-                                <td class="user-item"><?= $post['id'];?></td>
-                                <td class="user-item"><?= $post['title'];?></td>
-                                <td class="user-item"><?= $post['description'];?></td>
-                                <td class="user-item"><?= $post['cover'];?></td>
-                                <td class='user-item'><?= $post['user_id'];?></td>
-                                <td class='user-item'><?= $post['created_at'];?></td>
+                                <td class="user-item"><?= $post->id;?></td>
+                                <td class="user-item"><?= $post->title;?></td>
+                                <td class="user-item"><?= $post->description;?></td>
+                                <!-- <td class="user-item"><?= $post->cover;?></td> -->
+                                <td class='user-item'><?= $post->user->name;?></td>
+                                <td class='user-item'><?= date('d/m/Y', strtotime($post->created_at));?></td>
                                 
                                 <td class="user-item">
-                                    <a href="<?=$base;?>/post/<?= $post['id'];?>/editPost"><img src="<?=$base?>/assets/img/img_admin/edit.png" width="20px" height="20px" alt=""></a>
-                                    <a href="<?=$base;?>/post/<?= $post['id'];?>/deletePost" onclick="confirm('Tem certeza que deseja excluir o Post <?= $post['title'];?>')"><img src="<?=$base?>/assets/img/img_admin/del.png" width="20px" height="20px" alt=""></a>
+                                    <a href="<?=$base;?>/post/<?= $post->id;?>/editPost"><img src="<?=$base?>/assets/img/img_admin/edit.png" width="20px" height="20px" alt=""></a>
+                                    <a href="<?=$base;?>/post/<?= $post->id;?>/deletePost" onclick="confirm('Tem certeza que deseja excluir o Post <?= $post->title;?>')"><img src="<?=$base?>/assets/img/img_admin/del.png" width="20px" height="20px" alt=""></a>
                                 </td>
                                 
                             </tr>
