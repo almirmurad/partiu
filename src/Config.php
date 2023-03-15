6cf62654baf7f2714eccb2c5e4f7724eebf4e58a
almirmurad <q>
2023-03-14 21:36:24 -0300

    config pro local e hospedagem

--- src/Config.php
+++ src/Config.php
@@ -1,6 +1,18 @@
 <?php
 namespace src;
+//no server
+// class Config {
+//     const BASE_DIR = '/partiu/public';
 
+//     const DB_DRIVER = 'mysql';
+//     const DB_HOST = 'localhost';
+//     const DB_DATABASE = 'u396083122_partiudb';
+//     CONST DB_USER = 'u396083122_partiu';
+//     const DB_PASS = '!1379AmoJu09!';
+
+//     const ERROR_CONTROLLER = 'ErrorController';
+//     const DEFAULT_ACTION = 'index';
+// }
 class Config {
     const BASE_DIR = '/partiu/public';
 
--- src/controllers/site/PacController.php
+++ src/controllers/site/PacController.php
@@ -6,7 +6,7 @@ use src\models\Package;
 use src\handlers\site\PackageSiteHandler;
 use src\handlers\site\SubCatsSiteHandler;
 use src\handlers\site\EventsSiteHandler;
-use src\handlers\site\BannerSiteHAndler;
+use src\handlers\site\BannerSiteHandler;
 
 class PacController extends ControllerSite{
     public function index() {
